116a989e659d5ff08af56fa1b62f2022a8675329
RobUXUI <roberto.munizaga@bootcampudd.cl>
2024-02-23 10:50:29 -0300

    inst axios / cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-
-
+import React from 'react';
 import Home from './views/home/Home';
 // import Header from './componentes/header/Header';
 import Footer from './componentes/footer/Footer';
 // import Approutes from "./Routes/Approutes";
-
-
-
+import axios from 'axios';
 
 function App() {
+  const products = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/products");
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  }
+
   return (
     <>
-    {/* <Header/> */}
-    <Home/>
-    <Footer/>
-     {/* <Approutes /> */}
+      <button onClick={products}>Click Me</button>
+      {/* <Header/> */}
+      <Home/>
+      <Footer/>
+      {/* <Approutes /> */}
     </>
   )
 }
